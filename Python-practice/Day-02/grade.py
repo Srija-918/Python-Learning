fcddb2021701686933571af7891ce20baf31c694
Srija-918 <25315a6605@aiml.sreenidhi.edu.in>
2026-06-05 23:07:58 +0530

    day 02-Conditional Statements

---
+++ Python-practice/Day-02/grade.py
@@ -0,0 +1,9 @@
+marks=float(input("Enter your marks: "))
+if(marks>=90):
+    print("A Grade")
+elif(marks>=70):
+    print("B Grade")
+elif(marks>=50):
+    print("C Grade")
+else:
+    print("Failed")
---
+++ Python-practice/Day-02/largest.py
@@ -0,0 +1,6 @@
+a=float(input("Enter a number:"))
+b=float(input("Enter another number:"))
+if(a>b):
+    print(a, "is larger")
+else:
+    print(b, "is larger")
